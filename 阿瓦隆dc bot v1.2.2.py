c668421be47518fb8a92a8e4e2bbc95f1d976a3c
TonionCatzz <dj1213121@gmail.com>
2025-08-05 23:03:55 +0800

    Update 阿瓦隆dc bot v1.2.2.py

--- 阿瓦隆dc bot v1.2.2.py
+++ 阿瓦隆dc bot v1.2.2.py
@@ -9,21 +9,7 @@ import asyncio
 import re
 import os
 from datetime import datetime
-from flask import Flask
-import threading
 
-########################
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "Bot is running."
-
-def run_web():
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-    
-threading.Thread(target=run_web).start()
 ######################################################
 
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -443,6 +429,7 @@ async def quit(ctx, member: discord.Member = None):
         await ctx.send(f"⚠️ 關閉 driver 時發生錯誤：{str(e)}")
 
 bot.run(TOKEN)
+
 
 
 
